afac905c9e9246c0deffd4ed20b11db9ed8cdbff
evangwalker <evangibsonwalker@gmail.com>
2025-01-16 22:30:24 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/Evan-Walker-Portfolio-Website",
+    base: "/Evan-Walker-Portfolio-Website/",
     build: {
         minify: "terser",
     },
